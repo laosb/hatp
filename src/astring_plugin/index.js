e95277535cca4cfb9b994bf6756ccdcaa23148b1
laosb <laosb@lasteam.net>
2017-01-30 14:47:57 +0800

    Fix string literals.

--- src/astring_plugin/index.js
+++ src/astring_plugin/index.js
@@ -3,17 +3,18 @@ import astring from 'astring';
 // Create a custom generator that inherits from Astring's default generator
 const customGenerator = Object.assign({}, astring.defaultGenerator, {
   Literal(node, state) {
-    if (node.raw != null && node.raw === node.value) {
-      state.output.write(node.raw);
+    if (node.raw != null) {
+      const first = node.raw.charAt(0);
+      const last = node.raw.charAt(node.raw.length - 1);
+      if (first === last) { // Not HaLang-specific string literals
+        state.output.write(node.raw);
+      } else { // HaLang-specific string literals
+        state.output.write(node.value);
+      }
     } else if (node.regex != null) {
       this.RegExpLiteral(node, state);
     } else {
-      const stred = JSON.stringify(node.value);
-      if (`"${node.value}"` === stred) {
-        state.output.write(node.value);
-      } else {
-        state.output.write(stred);
-      }
+      state.output.write(JSON.stringify(node.value));
     }
   },
 });
