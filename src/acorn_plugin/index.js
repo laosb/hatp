e0891a7d4e01d2d3fae4ed0f168c236eeae3e125
laosb <laosb@lasteam.net>
2017-01-30 16:20:18 +0800

    Support `from 'xxx' import xxx;` syntax.

--- src/acorn_plugin/index.js
+++ src/acorn_plugin/index.js
@@ -1,8 +1,12 @@
 import { plugins } from 'acorn';
 import readToken from './readToken';
 import readString from './readString';
+import parseStatement from './parseStatement';
+import parseFromImport from './parseFromImport';
 
 plugins.halang = function halangPlugin(parser) {
   parser.extend('readToken', readToken);
   parser.extend('readString', readString);
+  parser.extend('parseStatement', parseStatement);
+  parser.extend('parseFromImport', parseFromImport);
 };
