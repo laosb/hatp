bf4db74e4426a2e023ea19cdcfcf037bc45a83ab
laosb <laosb@lasteam.net>
2017-01-30 14:34:15 +0800

    Expose acorn and astring config.

--- src/index.js
+++ src/index.js
@@ -7,22 +7,22 @@ import generator from './astring_plugin';
 function convert(code, opts) {
   // Parse it into an AST and retrieve the list of comments
   const comments = [];
-  const ast = parse(code, {
+  const ast = parse(code, Object.assign({
     ecmaVersion: 6,
     /* Should also have a halangVersion option in our acorn. */
     locations: true,
     onComment: comments,
-    plugins: { halang: opts },
-  });
+    plugins: { halang: opts.ha },
+  }, opts.acorn));
   // Attach comments to AST nodes
   attachComments(ast, comments);
   // Format it into a code string
-  const formattedCode = astring(ast, {
+  const formattedCode = astring(ast, Object.assign({
     indent: '  ',
     lineEnd: '\n',
     comments: true,
     generator,
-  });
+  }, opts.astring));
   return formattedCode;
 }
 
