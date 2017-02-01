3f04cd5c7a43ed33697179fd8401f0ffec33d8b8
zsx <zsx@zsxsoft.com>
2017-02-01 17:08:33 +0800

    Finish readTokens

--- src/acorn_plugin/readTokens.js
+++ src/acorn_plugin/readTokens.js
@@ -1,5 +1,13 @@
 import { tokTypes as tt } from 'acorn';
 
+const makeCodeAt = array => array.map(p => p.charCodeAt());
+const multiCompare = (array, ...data) => array.some(key => data.every(d => d === key));
+
+const dots = makeCodeAt(['。', '．']);
+const lts = makeCodeAt(['《', '＜']);
+const gts = makeCodeAt(['》', '＞']);
+const slashs = makeCodeAt(['、', '／']);
+
 // ### Token reading
 
 // This is the function that is called to fetch the next token. It
@@ -9,15 +17,12 @@ import { tokTypes as tt } from 'acorn';
 //
 // All in the name of speed.
 //
-export function readToken_dot(inner) {
+export function readTokenDot(inner) {
   return function ha() {
     const next = this.input.charCodeAt(this.pos + 1);
     if (next >= 65296 && next <= 65305) return this.readNumber(true);
     const next2 = this.input.charCodeAt(this.pos + 2);
-    if (this.options.ecmaVersion >= 6 &&
-      ((next === 12290 && next2 === 12290) || (next === 65294 || next2 === 65294))
-      // 。 = 12290，． = 65294
-      ) {
+    if (this.options.ecmaVersion >= 6 && multiCompare(dots, next, next2)) {
       this.pos += 3;
       return this.finishToken(tt.ellipsis);
     }
@@ -25,17 +30,17 @@ export function readToken_dot(inner) {
   };
 }
 
-export function readToken_slash(inner) {
+export function readTokenSlash(inner) {
   return function ha() { // '/'
     const next = this.input.charCodeAt(this.pos + 1);
-    if (this.exprAllowed) { ++this.pos; return this.readRegexp(); }
-    if (next === 12289 || next === 65295) return this.finishOp(tt.assign, 2);
+    if (this.exprAllowed) { this.pos += 1; return this.readRegexp(); }
+    if (multiCompare(slashs, next)) return this.finishOp(tt.assign, 2);
     return inner.call(this);
   };
 }
 
-export function readToken_mult_modulo_exp(inner) {
-  return function ha(code)  { // '%*'
+export function readTokenMultModuloExp(inner) {
+  return function ha(code) { // '%*'
     if (!(code === 215 || code === 65290)) return inner.call(this, code);
     let next = this.input.charCodeAt(this.pos + 1);
     let size = 1;
@@ -43,7 +48,7 @@ export function readToken_mult_modulo_exp(inner) {
 
     // exponentiation operator ** and **=
     if (this.options.ecmaVersion >= 7 && (next === 215 || next === 65290)) {
-      ++size;
+      size += 1;
       tokentype = tt.starstar;
       next = this.input.charCodeAt(this.pos + 2);
     }
@@ -53,7 +58,7 @@ export function readToken_mult_modulo_exp(inner) {
   };
 }
 
-export function readToken_pipe_amp(inner) {
+export function readTokenPipeAmp(inner) {
   return function ha(code) { // '|&'
     if (!(code === 65372 || code === 65286)) return inner.call(this, code);
     const next = this.input.charCodeAt(this.pos + 1);
@@ -63,7 +68,7 @@ export function readToken_pipe_amp(inner) {
   };
 }
 
-export function readToken_caret(inner) {
+export function readTokenCaret(inner) {
   return function ha() { // '^'
     const next = this.input.charCodeAt(this.pos + 1);
     if (next === 65309) return this.finishOp(tt.assign, 2);
@@ -71,17 +76,11 @@ export function readToken_caret(inner) {
   };
 }
 
-export function readToken_plus_min(inner) {
-  return function ha(code)  { // '+-'
+export function readTokenPlusMin(inner) {
+  return function ha(code) { // '+-'
+    if (!(code === 65291 || code === 65293 || code === 8212)) return inner.call(this, code);
     const next = this.input.charCodeAt(this.pos + 1);
     if (next === code) {
-      if (next == 45 && this.input.charCodeAt(this.pos + 2) == 62 &&
-          lineBreak.test(this.input.slice(this.lastTokEnd, this.pos))) {
-        // A `-->` line comment
-        this.skipLineComment(3);
-        this.skipSpace();
-        return this.nextToken();
-      }
       return this.finishOp(tt.incDec, 2);
     }
     if (next === 65309) return this.finishOp(tt.assign, 2);
@@ -89,34 +88,34 @@ export function readToken_plus_min(inner) {
   };
 }
 
-export function readToken_lt_gt(inner) {
+export function readTokenLtGt(inner) {
+  const concatenated = lts.concat(gts);
   return function ha(code) { // '<>'
+    if (concatenated.indexOf(code) < 0) {
+      return inner.call(this, code);
+    }
     const next = this.input.charCodeAt(this.pos + 1);
     let size = 1;
     if (next === code) {
-      size = code === 62 && this.input.charCodeAt(this.pos + 2) === 62 ? 3 : 2;
-      if (this.input.charCodeAt(this.pos + size) === 61) return this.finishOp(tt.assign, size + 1);
+      size = gts.indexOf(code) >= 0 && this.input.charCodeAt(this.pos + 2) === code ? 3 : 2;
+      if (lts.indexOf(this.input.charCodeAt(this.pos + size)) >= 0) {
+        return this.finishOp(tt.assign, size + 1);
+      }
       return this.finishOp(tt.bitShift, size);
     }
-    if (next == 33 && code == 60 && this.input.charCodeAt(this.pos + 2) == 45 &&
-        this.input.charCodeAt(this.pos + 3) == 45) {
-      if (this.inModule) this.unexpected();
-      // `<!--`, an XML-style comment that should be interpreted as a line comment
-      this.skipLineComment(4);
-      this.skipSpace();
-      return this.nextToken();
-    }
-    if (next === 61) size = 2;
+    if (lts.indexOf(next) >= 0) size = 2;
     return this.finishOp(tt.relational, size);
   };
 }
 
-export function readToken_eq_excl(inner) {
+export function readTokenEqExcl(inner) {
   return function ha(code) { // '=!'
     if (code !== 65309 && code !== 65281) return inner.call(this, code);
     const next = this.input.charCodeAt(this.pos + 1);
-    if (next === 65309) return this.finishOp(tt.equality, this.input.charCodeAt(this.pos + 2) === 65309 ? 3 : 2);
-    if (code === 65309 && (next === 65310 || next === 12299) && this.options.ecmaVersion >= 6) { // '=>'
+    if (next === 65309) {
+      return this.finishOp(tt.equality, this.input.charCodeAt(this.pos + 2) === 65309 ? 3 : 2);
+    }
+    if (code === 65309 && multiCompare(gts, next) && this.options.ecmaVersion >= 6) { // '=>'
       this.pos += 2;
       return this.finishToken(tt.arrow);
     }
