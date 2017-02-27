e2bff214c4773626816d3f58d19a2a84451c333d
zsx <zsx@zsxsoft.com>
2017-02-27 20:27:43 +0800

    Fix the bug hidden in astring plugin

--- src/astring_plugin/index.js
+++ src/astring_plugin/index.js
@@ -4,17 +4,16 @@ import astring from 'astring';
 const customGenerator = Object.assign({}, astring.defaultGenerator, {
   Literal(node, state) {
     if (node.raw != null) {
-      const first = node.raw.charAt(0);
-      const last = node.raw.charAt(node.raw.length - 1);
+      const first = node.raw.charAt(0).charCodeAt();
+      const last = node.raw.charAt(node.raw.length - 1).charCodeAt();
+
       if ((first === 12300 && last === 12301) // 「」
         || (first === 8216 && last === 8217)) { // ‘’
         state.output.write(`'${node.raw.slice(1, node.raw.length - 1)}'`);
-      }
-      if ((first === 12302 && last === 12303) // 『』
+      } else if ((first === 12302 && last === 12303) // 『』
         || (first === 8220 && last === 8221)) { // “”
         state.output.write(`"${node.raw.slice(1, node.raw.length - 1)}"`);
-      }
-      if ((first === 34 && last === 34)
+      } else if ((first === 34 && last === 34)
         || (first === 39 && last === 39)) { // "" or ''
         state.output.write(node.raw);
       } else { // HaLang-specific string literals
